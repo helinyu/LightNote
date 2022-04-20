be5409961393bf4b8a6f14f3d1fb80123a912bcf
helinyu <2319979647@qq.com>
2022-04-20 12:32:46 +0800

    快捷键的设置

--- src/background/index.js
+++ src/background/index.js
@@ -1,5 +1,5 @@
 chrome.action.onClicked.addListener((tab) => {
-  console.log("llt onClicked ---- tag",tab);
+  console.log("llt onClicked ---- tag", tab);
   if (tab.id) {
     chrome.tabs.sendMessage(tab.id, { toggleVisible: true });
   }
@@ -7,31 +7,33 @@ chrome.action.onClicked.addListener((tab) => {
 
 // trigger when active tab switches
 chrome.tabs.onActivated.addListener(function (activeInfo) {
-  console.log("llt onActivated ---- tag",activeInfo);
+  console.log("llt onActivated ---- tag", activeInfo);
   if (activeInfo?.tabId) {
     chrome.tabs.sendMessage(activeInfo?.tabId, { updateStorage: true });
   }
 });
 
-
-
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create(
     {
-      type: 'normal',
-      title: 'LightNote',
-      id: 'light-note',
-      contexts: ['all']
+      type: "normal",
+      title: "添加到LightNote (Ctrl+S)",
+      id: "light-note",
+      contexts: ["all"],
     },
     function () {
-      console.log('contextMenus are create.')
+      console.log("contextMenus are create.");
     }
-  )
-})
+  );
+});
+
 
 chrome.contextMenus.onClicked.addListener(info => {
   chrome.tabs.create({
-    url:'https://github.com/helinyu/LightNote/issues?q=' +encodeURI(info.selectionText)
-    // url:'https://github.com/helinyu/LightNote/issues'
+    url:'https://github.com/helinyu/LightNote/issues?q=' + encodeURI(info.selectionText)
   })
 })
+
+chrome.commands.onCommand.addListener((command) => {
+  console.log(`Command: ${command}`);
+});
